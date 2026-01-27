11b45060754dc9bcf2cc580a3abf4252c3946977
Jaymare <lenehan383@gmail.com>
2026-01-27 14:26:00 +1030

    retstructure files

--- scripts.js
+++ scripts.js
@@ -6,10 +6,19 @@ function init() {
   var dataset;
 
   // Data should appear in dev tools
-  d3.csv("data.csv", function (d) {}).then(function (data) {
-    dataset = data;
-    console.log("Data successfully loaded:", data);
+  d3.csv("data/oecd_data.csv", function (d) {}).then(function (data) {
+    // Create the visualisations
+    createVisualization1(data);
+    // createVisualization2(data);
+    // createVisualization3(data);
+    // createVisualization4(data);
+    // createVisualization5(data);
   });
+}
+
+function createVisualization1(data) {
+  dataset = data;
+  console.log("Data successfully loaded:", data);
 }
 
 window.onload = init;
