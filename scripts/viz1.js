c28bbe50c4f6c96e9ac3bdb4b72097d905ac86ec
tsaiyou78-rgb <tsaiyou78@gmail.com>
2026-02-11 13:25:08 +1100

    Update viz1.js

--- scripts/viz1.js
+++ scripts/viz1.js
@@ -1,70 +1,51 @@
 const width = 700;
 const height = 400;
-const cols = 5;
-const cellSize = 120;
 
-const svg = d3
-  .select("#chart")
-  .append("svg")
+const container = d3.select("#viz1"); // Changed from #chart
+if (container.empty()) return;
+
+const svg = container.append("svg")
   .attr("width", width)
   .attr("height", height);
 
-const tooltip = d3
-  .select("body")
+const tooltip = d3.select("body")
   .append("div")
   .attr("class", "tooltip")
   .style("opacity", 0);
 
-d3.csv("data/life_expectancy.csv").then((data) => {
-  // Filter to life expectancy only
+d3.csv("data/OECD_health_data.csv").then((data) => {
   const lifeExp = data.filter(
-    (d) => d.MEASURE === "LIFE_EXP" && d.OBS_VALUE !== "",
+    (d) => d.MEASURE === "LFEXP" && d.OBS_VALUE !== "" // Changed to LFEXP
   );
 
   lifeExp.forEach((d) => {
     d.OBS_VALUE = +d.OBS_VALUE;
   });
 
-  const colorScale = d3
-    .scaleSequential()
+  const colorScale = d3.scaleSequential()
     .domain(d3.extent(lifeExp, (d) => d.OBS_VALUE))
     .interpolator(d3.interpolateBlues);
 
-  svg
-    .selectAll("rect")
+  const regions = lifeExp.map(d => d["Reference area"]);
+  const xScale = d3.scaleBand()
+    .domain(regions)
+    .range([0, width])
+    .padding(0.1);
+
+  svg.selectAll("rect")
     .data(lifeExp)
     .enter()
     .append("rect")
-    .attr("class", "region")
-    .attr("x", (d, i) => (i % cols) * cellSize)
-    .attr("y", (d, i) => Math.floor(i / cols) * cellSize)
-    .attr("width", cellSize - 10)
-    .attr("height", cellSize - 10)
-    .attr("fill", (d) => colorScale(d.OBS_VALUE))
+    .attr("x", d => xScale(d["Reference area"]))
+    .attr("y", 0)
+    .attr("width", xScale.bandwidth())
+    .attr("height", 50)
+    .attr("fill", d => colorScale(d.OBS_VALUE))
     .on("mouseover", (event, d) => {
-      tooltip
-        .style("opacity", 1)
-        .html(
-          `
-          <strong>${d["Reference area"]}</strong><br/>
-          Life expectancy: ${d.OBS_VALUE} years
-        `,
-        )
+      tooltip.style("opacity", 1)
+        .html(`${d["Reference area"]}: ${d.OBS_VALUE} years`)
         .style("left", event.pageX + 10 + "px")
         .style("top", event.pageY - 28 + "px");
     })
-    .on("mouseout", () => {
-      tooltip.style("opacity", 0);
-    });
-
-  svg
-    .selectAll("text")
-    .data(lifeExp)
-    .enter()
-    .append("text")
-    .attr("x", (d, i) => (i % cols) * cellSize + 10)
-    .attr("y", (d, i) => Math.floor(i / cols) * cellSize + 20)
-    .text((d) => d.REF_AREA)
-    .attr("fill", "#000")
-    .attr("font-size", "12px");
+    .on("mouseout", () => tooltip.style("opacity", 0));
 });
